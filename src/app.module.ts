e7600eafb9f826d13dfecb4912f92193db021cbf
Ruban Mikhail <m.ruban@hh.ru>
2025-04-04 23:49:10 +0200

    correct app start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from 'src/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from 'src/auth/auth.module';
 import { AppController } from 'src/app.controller';
 import { GlobalJwtModule } from 'src/jwt/jwt.module';
 import { UploadModule } from 'src/upload/upload.module';
+import { DataBaseOptions } from 'src/configs/db.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync(DataBaseOptions),
     GlobalJwtModule,
     AuthModule,
     UploadModule,
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
-import { AppModule } from './app.module';
+import { AppModule } from 'src/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.set('trust proxy', true);
   app.use(helmet());
   await app.listen(3000);
 }
